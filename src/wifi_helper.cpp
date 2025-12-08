10e125601d9540b50751d24b4eb5f98fc563e076
Julian Leitner <yjulian@outlook.de>
2025-12-08 16:21:44 +0100

    Fixed Typo Fixed Indentation Removed AP PW

--- src/wifi_helper.cpp
+++ src/wifi_helper.cpp
@@ -8,24 +8,21 @@
 bool connectWifi() {
     WiFiManager wifiManager;
 
-  // Check if the reset pin is LOW to reset WiFi settings
-  if (digitalRead(RESET_PIN) == LOW) {
-    Serial.println("Reset pin is LOW, resetting WiFi settings...");
-    
-    wifiManager.resetSettings();
-    delay(1000); // Wait a moment to ensure settings are cleared
-  }
+    // Check if the reset pin is LOW to reset WiFi settings
+    if (digitalRead(RESET_PIN) == LOW) {
+      Serial.println("Reset pin is LOW, resetting WiFi settings...");
+
+      wifiManager.resetSettings();
+      delay(1000); // Wait a moment to ensure settings are cleared
+    }
     
     // Automatically connect using saved credentials,
-    // if connection fails, it starts an access point with the specified name ( "AutoConnectAP"),
-    // if empty will auto generate SSID, if password is blank it will be anonymous AP (wm.autoConnect())
-    // then goes into a blocking loop awaiting configuration and will return success result
-
-    bool res = wifiManager.autoConnect(DEFAULT_WIFI_SSID, DEFAULT_WIFI_PASSWORD); // password protected ap
+    // if connection fails, it starts an access point with the specified name and password
+    bool res = wifiManager.autoConnect(DEFAULT_WIFI_SSID);
 
     if(!res) {
         Serial.println("Failed to connect");
-        // ESP.restart();
+        ESP.restart();
     } 
     else {
         //if you get here you have connected to the WiFi    
